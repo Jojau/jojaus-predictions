7bd5aecc01431147b5e28d02b48e338157d04ce2
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2025-02-17 22:10:55 +0100

    first commit

---
+++ public/data/predictions.js
@@ -0,0 +1,251 @@
+const data = {
+    "predictions": [
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Slingshot"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "27"
+                    },
+                    {
+                        "title": "28"
+                    },
+                    {
+                        "title": "29"
+                    }
+                ],
+                "prediction_window": 270,
+                "title": "Size of the fish eaten by the cat (cm)"
+            },
+            "name": "Fish"
+        },
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Talo"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "A green rupee"
+                    },
+                    {
+                        "title": "Nothing"
+                    }
+                ],
+                "prediction_window": 40,
+                "title": "What's under the stone?"
+            },
+            "name": "StoneRupee"
+        },
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Zelda"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "Odd"
+                    },
+                    {
+                        "title": "Even"
+                    }
+                ],
+                "prediction_window": 40,
+                "title": "Centh of the goats minigame time?"
+            },
+            "name": "Goats"
+        },
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Enter Faron"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "In front of his house"
+                    },
+                    {
+                        "title": "Down the path"
+                    }
+                ],
+                "prediction_window": 150,
+                "title": "Moi spawns..."
+            },
+            "name": "Moi"
+        },
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Enter EMS"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "1"
+                    },
+                    {
+                        "title": "2"
+                    },
+                    {
+                        "title": "3"
+                    },
+                    {
+                        "title": "4"
+                    },
+                    {
+                        "title": ">5"
+                    }
+                ],
+                "prediction_window": 240,
+                "title": "Tries to get EMS"
+            },
+            "name": "EMS"
+        },
+        {
+            "auto_predict": {
+                "auto_start": false,
+                "split_name": "Enter Sacred Grove"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "Yeah"
+                    },
+                    {
+                        "title": "Nah"
+                    }
+                ],
+                "prediction_window": 150,
+                "title": "Will a bird hit me on the rooftops?"
+            },
+            "name": "ZeldaBirds"
+        },
+        {
+            "auto_predict": {
+                "auto_start": false,
+                "split_name": "Master Sword"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "1"
+                    },
+                    {
+                        "title": "2"
+                    },
+                    {
+                        "title": "3"
+                    },
+                    {
+                        "title": "4"
+                    }
+                ],
+                "prediction_window": 120,
+                "title": "Sidehops to clip through Lanayru Gate?"
+            },
+            "name": "LanayruGateClip"
+        },
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Enter Forest Temple"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "Yes"
+                    },
+                    {
+                        "title": "No"
+                    }
+                ],
+                "prediction_window": 270,
+                "title": "Backflip?"
+            },
+            "name": "Backflip"
+        },
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Gale"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "2"
+                    },
+                    {
+                        "title": "3"
+                    },
+                    {
+                        "title": "4"
+                    },
+                    {
+                        "title": ">5"
+                    }
+                ],
+                "prediction_window": 300,
+                "title": "Ook hops?"
+            },
+            "name": "Ook"
+        },
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Enter Diababa"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "A blue rupee"
+                    },
+                    {
+                        "title": "A heart"
+                    },
+                    {
+                        "title": "Nothing"
+                    }
+                ],
+                "prediction_window": 60,
+                "title": "The boko drops..."
+            },
+            "name": "BokoDrop"
+        },
+        {
+            "auto_predict": {
+                "auto_start": true,
+                "split_name": "Eldin Vessel"
+            },
+            "data": {
+                "broadcaster_id": "",
+                "outcomes": [
+                    {
+                        "title": "Item get (\"tatatadada!\")"
+                    },
+                    {
+                        "title": "Rupee get (\"HmHmHmm!\")"
+                    }
+                ],
+                "prediction_window": 60,
+                "title": "The postman sings..."
+            },
+            "name": "EldinPostman"
+        }
+    ]
+}
